bf9942e536f045b5c3b49684c9e8b6fe88f7e1cd
yosef <yosfemyau@gmail.com>
2025-07-26 18:00:53 +0300

    fixe job title selection

--- ethiojobsScraper/ethiojobsScraper/spiders/ethiojobs.py
+++ ethiojobsScraper/ethiojobsScraper/spiders/ethiojobs.py
@@ -14,25 +14,34 @@ class EthiojobsSpider(scrapy.Spider):
         print(f"Found {len(job_cards)} job listings")
 
         for job in job_cards:
+            """
+            <a href="/job/Xedb0EIl4z-hr-officer"><p class="MuiTypography-root MuiTypography-body1 mui-style-tz3at">HR Officer</p></a>
+            """    
+            main = job.css("div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-9.mui-style-rrl33y")
+            print("=======================")
+            print(main.css("a ::text").get())
+            print("====+==-0==3===============")
+            print(job.css("p.MuiTypography-root.MuiTypography-body1.mui-style-10rtjdg::text").get()),
+
             yield {
-                "title": job.xpath(
-                    ".//a[p[contains(@class,'MuiTypography-body1')]][last()]/p/text()"
-                ).get(),
+                "title": main.css("a ::text").get(),
+             
+                
                 "company": job.xpath(
                     ".//div[contains(@class,'MuiGrid-container')][.//p[contains(text(),'by')]]//a/button/text()"
                 ).get(),
+                "about": job.css("p.MuiTypography-root.MuiTypography-body1.mui-style-10rtjdg::text").get(),
+
                 "location": job.xpath(
                     ".//img[@alt='location']/following-sibling::p/text()"
                 ).get(),
-                "posted": job.xpath(
+                "Deadline": job.xpath(
                     ".//img[@alt='employment']/following-sibling::p/text()"
                 ).get(),
-            }
-            """
-              <button class="MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-colorPrimary MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-colorPrimary mui-style-11hluru" tabindex="0" type="button" style="background-color: inherit;"><p class="MuiTypography-root MuiTypography-body1 mui-style-okbma4" style="font-size: 13px; color: rgb(0, 0, 0); text-transform: none;">Next</p><span class="MuiTouchRipple-root mui-style-w0pj6f"></span></button>
+                "url": main.css("a::attr(href)").get(),
 
-            """
-            #  https://ethiojobs.net/jobs?page=2&isFeatured=false
+            }
+            
         pagination_list = response.xpath('//button[starts-with(@aria-label, "Go to page") or starts-with(@aria-label, "page ")]')
         last_page = pagination_list[-1].xpath("./@aria-label").get().strip().split()[-1]
  
@@ -44,4 +53,3 @@ class EthiojobsSpider(scrapy.Spider):
             yield scrapy.Request(
                 next_page_url, callback=self.parse, meta={"playwright": True}
             )
-        print("Finished scraping all pages.")
